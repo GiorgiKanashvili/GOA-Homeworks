f86632e35b0335aac0ecf17ce38ab7934212952b
GiorgiKanashvili <kanashviligiorgi9@gmail.com>
2024-08-03 00:20:20 +0400

    thirteenth commit

---
+++ Level 12/Classwork/level12.py
@@ -0,0 +1,26 @@
+# age = int(input("Enter your age: "))
+
+# if age <18:
+#     print("make a discount")
+# else:
+#     print("don't make a discount")
+
+# num1 = int(input("Enter the number: "))
+
+# if num1 == 6:
+#     print("you won a house")
+# elif num1 == 30:
+#     print("you won a house")
+# else:
+#     print("you won a dollar")
+
+age = int(input("enter your age: "))
+is_student = input("are you student?: ")
+yes = True
+no = False
+
+if age <18 or is_student:
+    print("make a discount")
+else:
+    print("dont make a discount")
+
