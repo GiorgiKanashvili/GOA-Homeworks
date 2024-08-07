74a653c40c4b4e7365f36faff74924e4d463dd28
GiorgiKanashvili <kanashviligiorgi9@gmail.com>
2024-08-07 15:12:13 +0400

    thirteenth commit

---
+++ Level 12/Homework/level12.py
@@ -0,0 +1,39 @@
+# i = 0
+
+# while i < 100:
+#      if i < 50 or i > 60:
+#          print(i)
+#      i += 1
+
+
+# num = int(input("Enter the number: "))
+
+# if num % 2 == 0:
+#     print("This number is even")
+# else:
+#     print("this number is odd")
+
+# num = int(input("enter the number: "))
+
+# if 90 <= num <= 100:
+#     print("grade A")
+# elif 80 <= num <= 89:
+#     print("grade B")
+# elif 70 <= num <=79:
+#     print("grade C")
+# elif 60 <= num <= 69:
+#     print("grade D")
+# else:
+#     print("grade F")
+
+age = int(input("Enter your age: "))
+
+if age < 13:
+    print("you are kid")
+elif age > 13 and age < 20:
+    print("you are teenager")
+elif age > 20:
+    print("you are grown")
+
+
+
